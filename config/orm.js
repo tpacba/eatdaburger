aef9c53eb24e79a1f1e8d4696415194e35615a9a
Tristan Pacba <tpacba@live.com>
2020-09-12 23:20:50 -0700

    finished orm.js, added the three functions that will be used

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,27 @@
 var connection = require("./connection.js");
 
 var orm = {
-    
+    selectAll: function(table, cb) {
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString,[table], function(err, data) {
+            if (err) throw err;
+            cb(data);
+        })
+    },
+    insertOne: function(table, col, vals, cb) {
+        var queryString = "INSERT INTO ?? (??) VALUES (??)";
+        connection.query(queryString, [table, col, vals], function(err, data) {
+            if (err) throw err;
+            cb(data);
+        })
+    },
+    updateOne: function(table, col, colCondition, valCondition, cb) {
+        var queryString = "UPDATE ?? SET ?? WHERE ?? = ?";
+        connection.query(queryString, [table, col, colCondition, valCondition], function(err, data) {
+            if (err) throw err;
+            cb(data);
+        })
+    }
 }
+
+module.exports = orm;
